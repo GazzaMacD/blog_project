c94c4077bd6a6fa730d0ff643ffdb0863ad31edf
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-10-28 12:02:53 +0200

    Basic home page, front and back, add core app to back

--- bk_blog/home/models.py
+++ bk_blog/home/models.py
@@ -1,19 +1,37 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.api import APIField
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
     banner_title = models.CharField(max_length=100, blank=False, null=False)
+    banner_intro = models.TextField(max_length=250, blank=False, null=False)
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        related_name="+",
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title"),
+        MultiFieldPanel(
+            [
+                FieldPanel("banner_title"),
+                FieldPanel("banner_intro"),
+                FieldPanel("banner_image"),
+            ],
+            heading="Home Top Banner",
+        )
     ]
 
     api_fields = [
         APIField("banner_title"),
+        APIField("banner_intro"),
+        APIField("banner_image"),
     ]
     max_count = 1
     parent_page_type = ["wagtailcore.Page"]
--- fr_blog/app/(front)/page.tsx
+++ fr_blog/app/(front)/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+
+const baseUrl = "http://127.0.0.1:8000";
+
 type TMeta = {
     type: string;
     detail_url: string;
@@ -10,6 +14,15 @@ type TMeta = {
     alias_of: string | null;
     locale: string;
 };
+type TImage = {
+    id: number;
+    meta: {
+        type: string;
+        detail_url: string;
+        download_url: string;
+    };
+    title: string;
+};
 type TData = {
     meta: { total_count: number };
     items: {
@@ -17,12 +30,18 @@ type TData = {
         meta: TMeta;
         title: string;
         banner_title: string;
+        banner_intro: string;
+        banner_image: TImage;
     }[];
+};
+
+const constructUrl = () => {
+    return ``;
 };
 
 async function getData(): Promise<TData> {
     const res = await fetch(
-        "http://127.0.0.1:8000/api/v2/pages/?slug=home&type=home.HomePage&fields=banner_title"
+        "http://127.0.0.1:8000/api/v2/pages/?slug=home&type=home.HomePage&fields=banner_title,banner_intro,banner_image"
     );
     return res.json();
 }
@@ -30,5 +49,16 @@ async function getData(): Promise<TData> {
 export default async function Page() {
     const data = await getData();
     const page = data.items[0];
-    return <h1>{page.banner_title}</h1>;
+    return (
+        <div>
+            <Image
+                src={`${baseUrl}${page.banner_image.meta.download_url}`}
+                alt={page.banner_image.title}
+                width={1039}
+                height={584}
+            />
+            <h1>{page.banner_title}</h1>
+            <p>{page.banner_intro}</p>
+        </div>
+    );
 }
