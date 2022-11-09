62bfbf1313e68de6956b8a1c61d438be17e57dd4
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-11-09 15:09:11 +0100

    Add blog posts to side bar

--- bk_blog/blog/admin.py
+++ bk_blog/blog/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from .models import BlogDetailPage
 
-# Register your models here.
+
+class BlogDetailAdmin(ModelAdmin):
+    model = BlogDetailPage
+    list_display = ("title", "published_date")
+    menu_order = 200
+
+
+modeladmin_register(BlogDetailAdmin)
